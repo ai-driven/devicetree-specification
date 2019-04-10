00752c1abd3e4f9de8484b7e7122f0c9b7766e57
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-10 21:26:06 +0000

    Deploy devicetree-org/devicetree-specification to github.com/devicetree-org/devicetree-specification.git:gh-pages

---
+++ _static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v0.2-18-g8466c3b',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
